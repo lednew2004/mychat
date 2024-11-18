a34b9f3319fc77d44c3284cdd69292a5b44be80a
wendel <wendellkaua870@gmail.com>
2024-11-18 19:15:19 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -27,15 +27,14 @@ const colors = [
 const user = {id: "", name: "", color: ""};
 let webSocket;
 
-// Função para criar mensagem própria
 function createMessageSelfElement(content){
     const div = document.createElement("div");
     div.classList.add("message_self");
-    div.innerHTML = content;
-    return div;
-}
+    div.innerHTML = content
 
-// Função para criar mensagem de outro usuário
+    return div;
+};
+
 function createMessageOtherement(content, sender, senderColor){
     const div = document.createElement("div");
     const span = document.createElement("span");
@@ -43,42 +42,41 @@ function createMessageOtherement(content, sender, senderColor){
     div.classList.add("message_other");
     div.classList.add("message_self");
     span.classList.add("message_send");
-    span.style.color = senderColor;
+    span.style.color = senderColor
 
     div.appendChild(span);
     span.innerHTML = sender;
-    div.innerHTML += content;
+    div.innerHTML+= content;
 
     return div;
-}
+};
 
-// Função para pegar uma cor aleatória para o usuário
+
 function getColor(){
     const indexColor = Math.floor(Math.random() * colors.length);
     return colors[indexColor];
-}
+};
 
-// Função para rolar para o fundo da tela
 function scrollScreen(){
     window.scrollTo({
         top: document.body.scrollHeight,
         behavior: "smooth"
-    });
-}
+    })
+};
 
-// Função para processar a mensagem recebida
-function processMessage({ data }) {
-    const { userId, userName, userColor, content } = JSON.parse(data);
+function processMessage({ data }){
+    const { userId, userName, userColor, content} = JSON.parse(data);
+
     const message = 
         userId === user.id 
         ? createMessageSelfElement(content)
-        : createMessageOtherement(content, userName, userColor);
+        :createMessageOtherement(content, userName, userColor);
 
     chatMessages.appendChild(message);
-    scrollScreen();
-}
 
-// Função de login (quando o usuário submete o formulário)
+    scrollScreen();
+};
+
 function loginSubmit(event){
     event.preventDefault();
 
@@ -89,51 +87,28 @@ function loginSubmit(event){
     login.style.display = "none";
     chat.style.display = "flex";
 
-    // Criar o WebSocket
-    webSocket = new WebSocket("wss://mychat-1kbw.onrender.com");
+    webSocket = new WebSocket("ws://localhost:8080");
     webSocket.onmessage = processMessage;
-    
-    // Salvar o nome no sessionStorage
-    localStorage.setItem("name", loginInput.value);
-}
 
-// Função para recuperar o nome do usuário do sessionStorage
-function storageSave(){
-    const nameStorage = localStorage.getItem("name");
-    if(nameStorage){
-        user.name = nameStorage;
-        login.style.display = "none";
-        chat.style.display = "flex";
+};
 
-        // Caso já tenha o nome, você pode estabelecer a conexão do WebSocket
-        webSocket = new WebSocket("wss://mychat-1kbw.onrender.com");
-        webSocket.onmessage = processMessage;
-    }
-}
 
-// Função para enviar a mensagem
+
 function sendMessage(event){
     event.preventDefault();
 
-    // Verificar se o WebSocket está aberto antes de tentar enviar
-    if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-        const messages = {
-            userId : user.id,
-            userName : user.name,
-            userColor : user.color,
-            content: chatInput.value
-        };
+    const messages = {
+        userId : user.id,
+        userName : user.name,
+        userColor : user.color,
+        content: chatInput.value
+    };
 
-        webSocket.send(JSON.stringify(messages));
-        chatInput.value = "";
-    } else {
-        console.error("WebSocket não está aberto.");
-    }
-}
+    webSocket.send(JSON.stringify(messages));
 
-// Chamar a função storageSave() para verificar se o nome já está no sessionStorage
-storageSave();
+    chatInput.value = "";
+};
 
-// Adicionar eventos de submit para login e envio de mensagens
+
 loginForm.addEventListener("submit", loginSubmit);
 chatForm.addEventListener("submit", sendMessage);
